cfae8563ebae37906055efb0fdba4f6a11c8a136
Lisa Kula <lisa.kula@offmadisonave.com>
2013-05-06 14:57:52 -0700

    css support for form elements

--- js/main.js
+++ js/main.js
@@ -1,1 +1,3 @@
-
+$(function() {
+  $("label").inFieldLabels();
+});
--- searchform.php
+++ searchform.php
@@ -1,8 +1,8 @@
-<form action="<?php bloginfo('siteurl'); ?>" id="searchform" method="get">
-    <div>
+<form action="<?php bloginfo('siteurl'); ?>" id="searchform" method="get" class="search-form">
+    <p>
         <label for="s" class="screen-reader-text">Search for:</label>
-        <input type="search" id="s" name="s" value="" />
+        <input type="text" id="s" name="s" value="" />
         
         <input type="submit" value="Search" id="searchsubmit" />
-    </div>
+    </p>
 </form>
